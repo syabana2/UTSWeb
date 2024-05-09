44b4cf29c0b42ca30a1a45a2acdced38466c8eeb
Syaban <rizkisyaban2@gmail.com>
2024-05-09 17:17:55 +0700

    fix some code

--- chart.php
+++ chart.php
@@ -1,57 +1,54 @@
-<div class="row">   
- <div class="col-lg-12">   
-   <h1 class="page-header">   
-     Chart <small>Statistics Overview</small>       
-   </h1>
+<div class="row">
+ <div class="col-lg-12">
    <ol class="breadcrumb">
-     <li class="active">     
+     <li class="active">
        <i class="fa fa-bar-chart"></i> Chart
      </li>
    </ol>
- </div>    
-</div>        
+ </div>
+</div>
 
-<?php               
+<?php
 //$con = mysqli_connect("localhost","root","","toko");
 
 $warna1 = "red";
-$warna2 = "green"; 
-$warna3 = "blue";     
-$warna4 = "orange";      
-$warna5 = "brown";          
+$warna2 = "green";
+$warna3 = "blue";
+$warna4 = "orange";
+$warna5 = "brown";
 
 //ambil data tabel yang ada di database
 // dan ambil jumlah record dari setiap tabel tersebut
 $i = 1;
 $txt = "";
-$jml = 0;    
+$jml = 0;
 $warna = "brown";
 $h = mysqli_query($con, "show tables from simup");
-while($r = mysqli_fetch_array($h)){                 
+while($r = mysqli_fetch_array($h)){
   if(($r[0] != "user") && ($r[0] != "tw_tabel") && ($r[0] != "tw_hak_akses") && ($r[0] != "logtw") && ($r[0] != "setting")){
   $txt1 = "\"". $r[0] ."\"";
   $x = mysqli_query($con, "select * from ". $r[0]);
   $jml1 = mysqli_num_rows($x);
   if($txt == ""){
     $txt = $txt1;
-    $jml = $jml1;   
+    $jml = $jml1;
     $warna = "\"". $warna1 ."\"";
-  }else{                            
-    $txt = $txt .",". $txt1;           
-    $jml = $jml .",". $jml1;              
+  }else{
+    $txt = $txt .",". $txt1;
+    $jml = $jml .",". $jml1;
     if($i == 1) { $warna = $warna .",\"". $warna1 ."\""; }
-    if($i == 2) { $warna = $warna .",\"". $warna2 ."\""; }   
-    if($i == 3) { $warna = $warna .",\"". $warna3 ."\""; }      
-    if($i == 4) { $warna = $warna .",\"". $warna4 ."\""; }         
-    if($i == 5) { $warna = $warna .",\"". $warna5 ."\""; }            
+    if($i == 2) { $warna = $warna .",\"". $warna2 ."\""; }
+    if($i == 3) { $warna = $warna .",\"". $warna3 ."\""; }
+    if($i == 4) { $warna = $warna .",\"". $warna4 ."\""; }
+    if($i == 5) { $warna = $warna .",\"". $warna5 ."\""; }
   }
   if($i < 5){ $i++; }else{ $i = 1; }
-  }                                    
-}                                         
-//echo $txt;                                 
-//echo $jml;                                    
-//echo $warna; 
-?>                
+  }
+}
+//echo $txt;
+//echo $jml;
+//echo $warna;
+?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 
@@ -59,25 +56,25 @@ while($r = mysqli_fetch_array($h)){
 
 <script>
 var xValues = [<?php echo $txt; ?>];
-var yValues = [<?php echo $jml; ?>];   
-var barColors = [<?php echo $warna; ?>];  
+var yValues = [<?php echo $jml; ?>];
+var barColors = [<?php echo $warna; ?>];
 
 new Chart("myChart", {
   type: "bar",
   data: {
     labels: xValues,
-    datasets: [{       
+    datasets: [{
       backgroundColor: barColors,
-      data: yValues                 
+      data: yValues
     }]
   },
   options: {
     legend: {display: false},
-    title: {                    
-      display: true,               
-      text: "Jumlah Data"             
-    }                                    
-  }                                         
-});  
+    title: {
+      display: true,
+      text: "Jumlah Data"
+    }
+  }
+});
 </script>
 
--- content.php
+++ content.php
@@ -1,276 +1,233 @@
 <?php
 session_start();
-if(!isset($_SESSION["Email"])){
-header("location:index.php");
+if (!isset($_SESSION["Email"])) {
+  header("location:index.php");
 }
 ?>
 
-<?php  
-include("db.php"); 
-include("header.php");  
-include("menu.php");    
-//ambil data setting 
+<?php
+include("db.php");
+include("header.php");
+include("menu.php");
+//ambil data setting
 $setting = mysqli_query($con, "select * from setting");
-while($rowSetting = mysqli_fetch_array($setting)){ 
-	$Nama = $rowSetting[1]; 
-	$Alamat = $rowSetting[2]; 
-	$Telepon = $rowSetting[3]; 
-	$Email = $rowSetting[4];
-	$Logo = $rowSetting["Logo"];
-}  
-?>      
-<div id="page-wrapper">   
- <div class="container-fluid">  
-<?php echo "<br>"; ?> 
- <div class="div-1"> 
-  <style>
-    .div-1 {
-      width : 70%;
-     border:solid black;
-    
-      margin: auto;
-    }
+while ($rowSetting = mysqli_fetch_array($setting)) {
+  $Nama = $rowSetting[1];
+  $Alamat = $rowSetting[2];
+  $Telepon = $rowSetting[3];
+  $Email = $rowSetting[4];
+  $Logo = $rowSetting["Logo"];
+}
+?>
+<div id="page-wrapper">
+  <div class="container-fluid">
+    <?php echo "<br>"; ?>
 
-    .statis {
-      font-family :'MANROPE';
-      text-decoration: under;
-    }
+    <!-- Page Heading -->
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="page-header new">Dashboard</h1>
+        <ol class="breadcrumb">
+          <li class="active">
+            <i class="fa fa-dashboard"></i> Dashboard
+          </li>
+        </ol>
+      </div>
+    </div>
 
-    .hijau {
-      
-    }
+    <div class="row">
+      <div class="col-lg-3 col-md-6">
+        <div class="panel panel-green">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-3">
+                <i class="fa fa-tasks fa-2x"></i>
+              </div>
+              <div class="col-xs-9 text-right">
+                <div class="huge">
+                  <?php
+                  $x = mysqli_query($con, "select count(*) as jumlah from dosen");
+                  while ($rx = mysqli_fetch_array($x)) {
+                    $total = $rx["jumlah"];
+                  }
+                  echo $total;
+                  ?>
+                </div>
+                <div class="statis">DOSEN</div>
+              </div>
+            </div>
+          </div>
+          <a href="listdosen.php">
+            <div class="panel-footer">
+              <span class="pull-left">View Details</span>
+              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+              <div class="clearfix"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="panel panel-red">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-3">
+                <i class="fa fa-tasks fa-5x"></i>
+              </div>
+              <div class="col-xs-9 text-right">
+                <div class="huge">
+                  <?php
+                  $x = mysqli_query($con, "select count(*) as jumlah from logtw");
+                  while ($rx = mysqli_fetch_array($x)) {
+                    $total = $rx["jumlah"];
+                  }
+                  echo $total;
+                  ?>
+                </div>
+                <div>LOGTW</div>
+              </div>
+            </div>
+          </div>
+          <a href="listlogtw.php">
+            <div class="panel-footer">
+              <span class="pull-left">View Details</span>
+              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+              <div class="clearfix"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="panel panel-yellow">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-3">
+                <i class="fa fa-tasks fa-5x"></i>
+              </div>
+              <div class="col-xs-9 text-right">
+                <div class="huge">
+                  <?php
+                  $x = mysqli_query($con, "select count(*) as jumlah from mahasiswa");
+                  while ($rx = mysqli_fetch_array($x)) {
+                    $total = $rx["jumlah"];
+                  }
+                  echo $total;
+                  ?>
+                </div>
+                <div>mahasiswa</div>
+              </div>
+            </div>
+          </div>
+          <a href="listmahasiswa.php">
+            <div class="panel-footer">
+              <span class="pull-left">View Details</span>
+              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+              <div class="clearfix"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-3">
+                <i class="fa fa-tasks fa-5x"></i>
+              </div>
+              <div class="col-xs-9 text-right">
+                <div class="huge">
+                  <?php
+                  $x = mysqli_query($con, "select count(*) as jumlah from program_studi");
+                  while ($rx = mysqli_fetch_array($x)) {
+                    $total = $rx["jumlah"];
+                  }
+                  echo $total;
+                  ?>
+                </div>
+                <div>program_studi</div>
+              </div>
+            </div>
+          </div>
+          <a href="listprogram_studi.php">
+            <div class="panel-footer">
+              <span class="pull-left">View Details</span>
+              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+              <div class="clearfix"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="panel panel-success">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-3">
+                <i class="fa fa-tasks fa-5x"></i>
+              </div>
+              <div class="col-xs-9 text-right">
+                <div class="huge">
+                  <?php
+                  $x = mysqli_query($con, "select count(*) as jumlah from setting");
+                  while ($rx = mysqli_fetch_array($x)) {
+                    $total = $rx["jumlah"];
+                  }
+                  echo $total;
+                  ?>
+                </div>
+                <div>setting</div>
+              </div>
+            </div>
+          </div>
+          <a href="listsetting.php">
+            <div class="panel-footer">
+              <span class="pull-left">View Details</span>
+              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+              <div class="clearfix"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <div class="row">
+              <div class="col-xs-3">
+                <i class="fa fa-tasks fa-5x"></i>
+              </div>
+              <div class="col-xs-9 text-right">
+                <div class="huge">
+                  <?php
+                  $x = mysqli_query($con, "select count(*) as jumlah from usulan_penelitian");
+                  while ($rx = mysqli_fetch_array($x)) {
+                    $total = $rx["jumlah"];
+                  }
+                  echo $total;
+                  ?>
+                </div>
+                <div>usulan_penelitian</div>
+              </div>
+            </div>
+          </div>
+          <a href="listusulan_penelitian.php">
+            <div class="panel-footer">
+              <span class="pull-left">View Details</span>
+              <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+              <div class="clearfix"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <!-- /.container-fluid -->
+    </div>
 
-  </style> 
-<table> 
-   <tr>    
-     <td>&nbsp;&nbsp;</td>
-     <td><?php echo "<img src='images/imagedata.png' width=300 height=220><br>"; ?></td>
-     <td>&nbsp;&nbsp;</td>
-     <td>
-       <?php
-         echo "<h1>&nbsp; ". $Nama ."</h1>";
-         echo "&nbsp;&nbsp;&nbsp;". $Alamat;
-         echo "<br>";
-         echo "&nbsp;&nbsp;&nbsp;". $Telepon;
-         echo " - ";
-         echo $Email;
-         echo "<br><br>";
-       ?>
-     </td>
-   </tr>
- </table>
- </div>  
-                <!-- Page Heading --> 
-                <div class="row">   
-                    <div class="col-lg-12">  
-                        <h1 class="page-header new"> 
-                    
-                        
-                        <style>
+    <?php echo "<br>"; ?>
+    <?php echo "<br>"; ?>
+    <?php echo "<br>"; ?>
 
-                          .new{
-                            margin-left: 40%;
-                          }
-                        </style>
-                        <small>Statistics Overview</small> 
-                        </h1>        
-                        <ol class="breadcrumb">   
-                            <li class="active">  
-                                <i class="fa fa-dashboard"></i>Dashboard    
-                            </li>                   
-                        </ol>    
-                    </div>          
-               </div>  
-                   
-<div class="row hijau">  
-<div class="col-lg-3 col-md-6 hijau">  
- <div class="panel panel-green">  
-   <div class="panel-heading"> 
-     <div class="row">       
-       <div class="col-xs-3">   
-         <i class="fa fa-tasks fa-2x"></i>  
-       </div>                       
-       <div class="col-xs-9 text-right">  
-         <div class="huge">       
-         <?php                    
-          $x=mysqli_query($con, "select count(*) as jumlah from dosen");    
-          while($rx = mysqli_fetch_array($x)){                
-            $total = $rx["jumlah"];        
-          } 
-          echo $total; 
-          ?>  
-         </div>  
-         <div class = "statis">DOSEN</div>   
-       </div>   
-      </div>   
-     </div>    
-     <a href="listdosen.php">  
-       <div class="panel-footer">  
-         <span class="pull-left">View Details</span> 
-         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>  
-         <div class="clearfix"></div>    
-       </div>                   
-     </a>                  
-    </div>                      
-   </div>                     
-<div class="col-lg-3 col-md-6">  
- <div class="panel panel-red">  
-   <div class="panel-heading"> 
-     <div class="row">       
-       <div class="col-xs-3">   
-         <i class="fa fa-tasks fa-5x"></i>  
-       </div>                       
-       <div class="col-xs-9 text-right">  
-         <div class="huge">       
-         <?php                    
-          $x=mysqli_query($con, "select count(*) as jumlah from logtw");    
-          while($rx = mysqli_fetch_array($x)){                
-            $total = $rx["jumlah"];        
-          } 
-          echo $total; 
-          ?>  
-         </div>  
-         <div>LOGTW</div>   
-       </div>   
-      </div>   
-     </div>    
-     <a href="listlogtw.php">  
-       <div class="panel-footer">  
-         <span class="pull-left">View Details</span> 
-         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>  
-         <div class="clearfix"></div>    
-       </div>                   
-     </a>                  
-    </div>                      
-   </div>                     
-<div class="col-lg-3 col-md-6">  
- <div class="panel panel-yellow">  
-   <div class="panel-heading"> 
-     <div class="row">       
-       <div class="col-xs-3">   
-         <i class="fa fa-tasks fa-5x"></i>  
-       </div>                       
-       <div class="col-xs-9 text-right">  
-         <div class="huge">       
-         <?php                    
-          $x=mysqli_query($con, "select count(*) as jumlah from mahasiswa");    
-          while($rx = mysqli_fetch_array($x)){                
-            $total = $rx["jumlah"];        
-          } 
-          echo $total; 
-          ?>  
-         </div>  
-         <div>mahasiswa</div>   
-       </div>   
-      </div>   
-     </div>    
-     <a href="listmahasiswa.php">  
-       <div class="panel-footer">  
-         <span class="pull-left">View Details</span> 
-         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>  
-         <div class="clearfix"></div>    
-       </div>                   
-     </a>                  
-    </div>                      
-   </div>                     
-<div class="col-lg-3 col-md-6">  
- <div class="panel panel-primary">  
-   <div class="panel-heading"> 
-     <div class="row">       
-       <div class="col-xs-3">   
-         <i class="fa fa-tasks fa-5x"></i>  
-       </div>                       
-       <div class="col-xs-9 text-right">  
-         <div class="huge">       
-         <?php                    
-          $x=mysqli_query($con, "select count(*) as jumlah from program_studi");    
-          while($rx = mysqli_fetch_array($x)){                
-            $total = $rx["jumlah"];        
-          } 
-          echo $total; 
-          ?>  
-         </div>  
-         <div>program_studi</div>   
-       </div>   
-      </div>   
-     </div>    
-     <a href="listprogram_studi.php">  
-       <div class="panel-footer">  
-         <span class="pull-left">View Details</span> 
-         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>  
-         <div class="clearfix"></div>    
-       </div>                   
-     </a>                  
-    </div>                      
-   </div>                     
-<div class="col-lg-3 col-md-6">  
- <div class="panel panel-success">  
-   <div class="panel-heading"> 
-     <div class="row">       
-       <div class="col-xs-3">   
-         <i class="fa fa-tasks fa-5x"></i>  
-       </div>                       
-       <div class="col-xs-9 text-right">  
-         <div class="huge">       
-         <?php                    
-          $x=mysqli_query($con, "select count(*) as jumlah from setting");    
-          while($rx = mysqli_fetch_array($x)){                
-            $total = $rx["jumlah"];        
-          } 
-          echo $total; 
-          ?>  
-         </div>  
-         <div>setting</div>   
-       </div>   
-      </div>   
-     </div>    
-     <a href="listsetting.php">  
-       <div class="panel-footer">  
-         <span class="pull-left">View Details</span> 
-         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>  
-         <div class="clearfix"></div>    
-       </div>                   
-     </a>                  
-    </div>                      
-   </div>                     
-<div class="col-lg-3 col-md-6">  
- <div class="panel panel-info">  
-   <div class="panel-heading"> 
-     <div class="row">       
-       <div class="col-xs-3">   
-         <i class="fa fa-tasks fa-5x"></i>  
-       </div>                       
-       <div class="col-xs-9 text-right">  
-         <div class="huge">       
-         <?php                    
-          $x=mysqli_query($con, "select count(*) as jumlah from usulan_penelitian");    
-          while($rx = mysqli_fetch_array($x)){                
-            $total = $rx["jumlah"];        
-          } 
-          echo $total; 
-          ?>  
-         </div>  
-         <div>usulan_penelitian</div>   
-       </div>   
-      </div>   
-     </div>    
-     <a href="listusulan_penelitian.php">  
-       <div class="panel-footer">  
-         <span class="pull-left">View Details</span> 
-         <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>  
-         <div class="clearfix"></div>    
-       </div>                   
-     </a>                  
-    </div>                      
-   </div>                     
-           <!-- /.container-fluid -->   
-</div>      
- 
-<?php   
-include("chart.php");  
-echo "<hr>";   
-?> 
- 
-<?php   
-include("footer.php");  
-?>        
+    <?php
+    include("chart.php");
+    echo "<hr>";
+    ?>
+
+    <?php
+    include("footer.php");
+    ?>
